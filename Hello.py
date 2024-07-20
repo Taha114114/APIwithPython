60bb012b23bd62d4acca4f0d095a2e333e978894
Taha114114 <61667719+Taha114114@users.noreply.github.com>
2024-07-20 11:51:11 +0300

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -3,3 +3,17 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World nnn"}
+
+@app.get("/rockpaperscissors")
+def rockpaperscissors(weight:float, hight:float):
+    bmi=weight/(hight**2)
+    if bmi<1:
+        message = "your bmi less than 1, eat more"
+    elif 1<=bmi<10:
+        message = "your bmi less than 10 enta keda tmam"
+    elif bmi>10:
+        message = "your bmi more than 10, batal akl"
+
+    
+    return {"your bmi":bmi, "message": message}
+
